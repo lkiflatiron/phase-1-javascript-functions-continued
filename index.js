4568af0445dbf8530cb245cb42a4bed1a5121eb0
Louise Iyengar <louise.iyengar@flatironschool.com>
2021-12-07 16:48:55 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(arg='roller-skate') {
+    return `This Saturday, I want to ${arg}!`
+}
+
+function mondayWork (act="go to the office") {
+    return `This Monday, I will ${act}.`
+}
+
+function wrapAdjective(visual="*") {
+    return function inner(adj="special") {
+        return `You are ${visual}${adj}${visual}!`;
+    }
+}
