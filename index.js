4edc540d91de2f42b646a53498c11f9613995ea4
Louise Iyengar <louise.iyengar@flatironschool.com>
2021-12-26 14:00:00 -0500

    Done

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ function saturdayFun(arg='roller-skate') {
     return `This Saturday, I want to ${arg}!`
 }
 
-function mondayWork (act="go to the office") {
-    return `This Monday, I will ${act}.`
+// function mondayWork (act="go to the office") {
+//     return `This Monday, I will ${act}.`
+// }
+
+// function wrapAdjective(visual="*") {
+//     return function inner(adj="special") {
+//         return `You are ${visual}${adj}${visual}!`;
+//     }
+// }
+
+const mondayWork = function (activity="go to the office") {
+    return `This Monday, I will ${activity}.`
 }
 
-function wrapAdjective(visual="*") {
-    return function inner(adj="special") {
-        return `You are ${visual}${adj}${visual}!`;
+function wrapAdjective(flair="*") {
+    return function (adj="special") {
+        return `You are ${flair}${adj}${flair}!`;
     }
 }
