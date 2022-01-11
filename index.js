b8b36ccf3007fe6d336098f7a4c6d0a2defd31ff
Louise Iyengar <louise.iyengar@flatironschool.com>
2022-01-10 22:28:08 -0500

    commit tree f047624c911636f8613bd00afcd8f214416f6b51

--- index.js
+++ index.js
@@ -3,16 +3,6 @@ function saturdayFun(arg='roller-skate') {
     return `This Saturday, I want to ${arg}!`
 }
 
-// function mondayWork (act="go to the office") {
-//     return `This Monday, I will ${act}.`
-// }
-
-// function wrapAdjective(visual="*") {
-//     return function inner(adj="special") {
-//         return `You are ${visual}${adj}${visual}!`;
-//     }
-// }
-
 const mondayWork = function (activity="go to the office") {
     return `This Monday, I will ${activity}.`
 }
